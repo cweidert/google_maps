a515b30b8af13ad2e3e1b468fec7724360bd81fe
Craig Weidert <craig.weidert@gmail.com>
2016-10-09 17:39:05 -0700

    Restructuring to have two projects in here.

---
+++ scripts/testPolygons.js
@@ -0,0 +1,9 @@
+polygons = {
+  firstPolygon: {
+    caption: "A caption",
+    paths: [[{lat: 34.056029, lng: -118.234763}, {lat: 34.055438, lng: -118.245269}, {lat: 34.048432, lng: -118.251592}]],
+    area: 100,
+    origins: 500,
+    destinations: 620
+  }
+}
